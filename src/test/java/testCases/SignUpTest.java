294733e5183b3e7ac16e47c38c1deef1e019321f
Pavan <gnpavan430@gmail.com>
2018-06-30 09:54:09 +0200

    Added a new log message

--- src/test/java/testCases/SignUpTest.java
+++ src/test/java/testCases/SignUpTest.java
@@ -94,7 +94,7 @@ public class SignUpTest extends NewSetUp {
             clientUtils.waitForElement(driver,personalDetails.personalDetailsTitle);
             logger.debug("Taking screenshot for personal details screen");
 
-            logger.debug("Added new linie");
+            logger.debug("Added new line");
             ++index;
             NewSetUp.getScreenshot(driver,index+"PersonalDetails",currentMethodName);
             WebElement typeOfParkingWebElement = personalDetails.selectTypeOfParking(driver,hashMap.get("Type of Parking").toString());
